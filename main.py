b41416b5b4f3cbe79a9fdd9964866e8da8f783a2
JavadFazelpour <Javad_Fazelpour@outlook.com>
2024-04-23 14:06:11 +0330

    Generate two random tiles with value 2 at the start of a new game.

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def draw_grid(window):
 
 def draw(window, tiles):
     window.fill(BACKGROUND_COLOR)
-    
+
     for tile in tiles.values():
         tile.draw(window)
 
@@ -90,11 +90,32 @@ def draw(window, tiles):
     pygame.display.update()
 
 
+def get_random_pos(tiles):
+    row = None
+    col = None
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+
+        if f"{row}{col}" not in tiles:
+            break
+    return row, col
+
+
+def generate_tiles():
+    tiles = {}
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+
+    return tiles
+
+
 def main(window):
     clock = pygame.time.Clock()
     run = True
 
-    tiles={}
+    tiles = generate_tiles()
 
     while run:
         clock.tick(FPS)
